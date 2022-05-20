4840a480178a0c6a77105ba796ae7286a54b5261
rajaime284 <rajaime284@gmail.com>
2022-05-20 17:44:33 +0200

    Con Bootstrap

--- taller-fe/src/App.vue
+++ taller-fe/src/App.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <div id="app">
     <Head />
--- taller-fe/src/components/Mislistas.vue
+++ taller-fe/src/components/Mislistas.vue
@@ -1,5 +1,44 @@
 <template>
    <div>
+      <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+      <!--
       <h1>{{ titulo }}</h1>
       <button v-if="mode!='add'" v-on:click="addMode()" class="btn btn-primary">
          {{$t('add.btnAdd')}}
@@ -78,7 +117,7 @@
             </div> 
             <div class="alert alert-danger" v-if="errAdd!=''">Error: {{errAdd}}</div>
          </fieldset>
-      </div>
+      </div>-->
    </div>
 </template>
 
@@ -90,6 +129,11 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import TaskList from '@/models/TaskList';
 import moment from 'moment';
+//import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+//Vue.use(BootstrapVue);
 
 @Component({
    filters: {
